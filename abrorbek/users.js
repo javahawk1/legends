dcbb8f5dea65ef384e57085f695db597050c9de7
Abrorbek <a05490156@gmail.com>
2025-07-10 17:51:22 +0500

    commit

---
+++ abrorbek/users.js
@@ -0,0 +1,27 @@
+
+
+
+
+
+
+const api = axios.create({
+    baseURL: "https://686faa8991e85fac42a2043f",
+
+});
+
+image = document.querySelector(".image")
+title = document.querySelector(".title");
+names  = document.querySelector(".names");
+malumot = document.querySelector(".malumot")
+
+
+ function showData(data) {
+    btn.textContent = "";
+    data.forEach(e => {
+        image: image.value
+        // title: title.value,
+        // names: names.value
+
+    });
+
+ }
